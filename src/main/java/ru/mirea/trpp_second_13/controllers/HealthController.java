d6e8d3de985d008bde38729bb78cf7f5aac84452
Sergey <ksergey263@gmail.com>
2021-04-23 10:13:21 +0300

    FINALLLEEEEEEEEE

--- src/main/java/ru/mirea/trpp_second_13/controllers/HealthController.java
+++ src/main/java/ru/mirea/trpp_second_13/controllers/HealthController.java
@@ -3,7 +3,7 @@ package ru.mirea.trppsecond13.controllers;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import ru.mirea.trpp_second_13.entity.HealthResponse;
+import ru.mirea.trppsecond13.entity.HealthResponse;
 
 /** Проверка состояния сервера. */
 @Controller()
